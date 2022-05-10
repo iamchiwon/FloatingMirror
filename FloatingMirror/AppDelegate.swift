cf422f555532d759b7b8428ceba680cedf4eac99
Song Chiwon <iamchiwon@gmail.com>
2022-05-11 00:13:10 +0900

    initialize project

---
+++ FloatingMirror/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  FloatingMirror
+//
+//  Created by Chiwon Song on 2022/05/10.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ FloatingMirror/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  FloatingMirror
+//
+//  Created by Chiwon Song on 2022/05/10.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
