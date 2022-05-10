fe075031b4b30daa094608d80f55e762d560ccb5
Song Chiwon <iamchiwon@gmail.com>
2022-05-11 00:25:54 +0900

    move window with dragging in view

--- FloatingMirror/ViewController.swift
+++ FloatingMirror/ViewController.swift
@@ -11,8 +11,33 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+
     override func viewDidAppear() {
         view.window?.level = .floating
+    }
+
+    override func mouseDown(with event: NSEvent) {
+        guard let window = view.window else { return }
+
+        let startingPoint = event.locationInWindow
+
+        window.trackEvents(matching: [.leftMouseDragged, .leftMouseUp], timeout: .infinity, mode: .default) { event, stop in
+            guard let event = event  else { return }
+            switch event.type {
+            case .leftMouseUp:
+                NSApp.postEvent(event, atStart: false)
+                stop.pointee = true
+
+            case .leftMouseDragged:
+                let currentPoint = event.locationInWindow
+                if abs(currentPoint.x - startingPoint.x) >= 5 || abs(currentPoint.y - startingPoint.y) >= 5 {
+                    stop.pointee = true
+                    window.performDrag(with: event)
+                }
+
+            default:
+                break
+            }
+        }
     }
 }
